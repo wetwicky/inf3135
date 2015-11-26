ab8ea57679a3064c0211fa9dbfdabf095c4596fa
Michael Vincent <michaelvincent2293@gmail.com>
2015-11-25 23:02:00 -0500

    tp2

--- tp2/main.c
+++ tp2/main.c
@@ -15,20 +15,24 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <malloc.h>
 
 typedef struct
 {
-    
+    char recette[120];
+    void *next;
 }recette_t ;
 
 typedef struct
 {
-    
+    char categorie[120];
+    recette_t *recette_t;
+    category_t *next;
 }category_t ;
 
 void createNewRecipy()
 {
-    
+    (category_t *)malloc();
 }
 
 void validationNombreDeParametre(int nombreDeParametre, char* fichierExecution)
@@ -51,21 +55,63 @@ FILE* ouvertureFichier(char* cheminFichier, char* droitAcces)
     return fichierAOuvrir;
 }
 
+char *substring(char *string, int position, int length) 
+{
+   char *pointer;
+   int c;
+ 
+   pointer = malloc(length+1);
+ 
+   if (pointer == NULL)
+   {
+      printf("Unable to allocate memory.\n");
+      exit(1);
+   }
+ 
+   for (c = 0 ; c < length ; c++)
+   {
+      *(pointer+c) = *(string+position-1);      
+      string++;   
+   }
+ 
+   *(pointer+c) = '\0';
+ 
+   return pointer;
+}
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-    char lineBuffer[81];
-    char *line_p = lineBuffer;
-    char *nomRecette[81];
-    char *nomCategory[81];
+    char lineBuffer[120];
+    char line_p = lineBuffer;
+    char nomRecette[120];
+    char nomCategory[120];
+    category_t *pointerForCategory;
+    int carac = 0;
+    int length = 0;
+    int position = 0;
     
     validationNombreDeParametre(argc, argv[0]);
-    FILE *dataBank = ouvertureFichier(argv[1], "w+");
+    FILE *dataBank = ouvertureFichier(argv[1], "r");
     while(!feof(dataBank))
     {
-        fgets(lineBuffer, 81, dataBank);
-        strchr(line_p,'[');
+        fgets(lineBuffer, 120, dataBank);
+        while(line_p != '[')
+        {
+            length++;
+            line_p++;
+        }
+        nomRecette = substring(lineBuffer, 0, length-1);
+        
+        position = length;
+        length = 0;
+        while(line_p != ']')
+        {
+            length++;
+            line_p++;
+        }
+        nomCategory = substring(lineBuffer, position+1, length-1);
     }
     rewind(dataBank);
     fclose(dataBank);

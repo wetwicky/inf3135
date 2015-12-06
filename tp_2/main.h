2934b02e56a732e359bc2e71d321cbbcf9d25b18
Michael <michaelvincent2293@gmail.com>
2015-12-06 17:45:38 -0500

    big progress

--- tp_2/main.h
+++ tp_2/main.h
@@ -15,7 +15,11 @@
 #define MAIN_H
 
 /*
- validate the number of parameter
+ * validate the number of parameter
+ * 
+ * @param:  category_t ** headOfCategory
+ * 
+ * @return: int for success
  */
 int validationNombreDeParametre (int, char*);
 int ouvertureFichier (FILE*, char*, char*);
--- tp_2/parameter.h
+++ tp_2/parameter.h
@@ -13,9 +13,25 @@
 
 #ifndef PARAMETER_H
 #define PARAMETER_H
-
+/*
+ * validate the number of parameter passed in argument.
+ * 
+ * @param:  int nombreDeParametre
+ *          char* fichierExecution
+ * 
+ * @return: int for success
+ */
 int validationNombreDeParametre (int nombreDeParametre, char* fichierExecution);
 
+/*
+ * make sure the name of the string is valid and open acces to the file.
+ * 
+ * @param:  FILE **fichierAOuvrir
+ *          char* cheminFichier
+ *          char* droitAcces
+ * 
+ * @return: int for success
+ */
 int ouvertureFichier (FILE **fichierAOuvrir, char* cheminFichier, char* droitAcces);
 
 #endif /* PARAMETER_H */
